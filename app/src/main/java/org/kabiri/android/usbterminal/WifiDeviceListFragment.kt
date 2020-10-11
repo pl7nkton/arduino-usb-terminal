cfbcc49f54499044f1b05d59e31977ea2d975188
Ali Kabiri <k4biri@icloud.com>
2020-10-11 23:45:45 +0200

    use the new compose layout to inflate the fragment

--- app/src/main/java/org/kabiri/android/usbterminal/WifiDeviceListFragment.kt
+++ app/src/main/java/org/kabiri/android/usbterminal/WifiDeviceListFragment.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
-import org.kabiri.android.usbterminal.databinding.FragmentWifiDeviceListBinding
-import org.kabiri.android.usbterminal.ui.adapter.WifiDeviceAdapter
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.kabiri.android.usbterminal.ui.WifiDeviceListScreen
 import org.kabiri.android.usbterminal.viewmodel.WifiDeviceListViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
  /**
  * Created by Ali Kabiri on 23.05.20.
  */
-class WifiDeviceListFragment: Fragment() {
+ @ExperimentalCoroutinesApi
+ class WifiDeviceListFragment: Fragment() {
 
     private val viewModel: WifiDeviceListViewModel by viewModel()
 
@@ -24,48 +24,18 @@ class WifiDeviceListFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
 
-        // inflate the fragment layout using bindings.
-        val binding = FragmentWifiDeviceListBinding
-            .inflate(inflater, container, false)
-        context ?: return binding.root
-        val placeHolder = binding.tvPlaceHolderText
-        val serverList = binding.wifiDeviceList
-
-        val adapter = WifiDeviceAdapter()
-        serverList.adapter = adapter
-
-        subscribeUi(adapter, serverList, placeHolder) // update the list whenever the dataSet changes.
-
-        return binding.root
+        // not the best way to use compose, but since we already created
+        // the prefs screen before, we'll keep it this way for now!
+        return inflater.inflate(R.layout.fragment_wifi_device_list, container, false)
+            .apply { findViewById<ComposeView>(R.id.compose_view).setContent {
+                WifiDeviceListScreen(
+                    viewModel = viewModel
+                )
+            } }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.handleDeviceMode(requireContext()) // read the device mode and react to it!
-    }
-
-    private fun subscribeUi(adapter: WifiDeviceAdapter,
-                            serverList: RecyclerView, placeholder: TextView) {
-        viewModel.observeDevices()
-        viewModel.wifiDevices.observe(viewLifecycleOwner, { wifiDevices ->
-            adapter.submitList(wifiDevices)
-        })
-        viewModel.showRemoteServers.observe(viewLifecycleOwner, {
-            // show a place holder for the list adapter saying that
-            // this device is a server for itself!
-            updatePlaceHolderVisibility(
-                showRemoteServers = it, serverList = serverList, placeholder = placeholder)
-        })
-    }
-
-    private fun updatePlaceHolderVisibility(showRemoteServers: Boolean,
-                                            serverList: RecyclerView, placeholder: TextView) {
-        if (showRemoteServers) {
-            placeholder.visibility = View.GONE
-            serverList.visibility = View.VISIBLE
-        } else {
-            placeholder.visibility = View.VISIBLE
-            serverList.visibility = View.GONE
-        }
     }
 }
